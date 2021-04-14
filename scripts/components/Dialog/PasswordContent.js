9bac69e8d27998795dee6b3b42147d638c939da6
eddiman <edbjorgen@gmail.com>
2021-04-14 16:44:33 +0200

    new design for prompt, updated icons, added animations for different states

--- scripts/components/Dialog/PasswordContent.js
+++ scripts/components/Dialog/PasswordContent.js
@@ -7,43 +7,76 @@ export default class PasswordContent extends React.Component {
     super(props);
     this.state = {
       unlocked: false,
-      hasClicked: false
+      hasClicked: false,
+      shakeClass : "",
+      showPasswordInput : true
     }
-
   }
+
   handleOnClick = event => {
     event.preventDefault();
     this.setState({
       hasClicked: true
     })
-    console.log(this.props.currentInteractionIndex)
     if(this.props.currentInteraction.unlocked) {
       this.props.showInteraction(this.props.currentInteractionIndex)
     } else {
       this.setState({
         unlocked: this.props.handlePassword(this.input.value)
       })
+      console.log("unlocked: " +this.state.unlocked)
+      if(!this.props.currentInteraction.unlocked){
+        this.shakeIcon();
 
-
+      }
     }
+  };
+  toggleShowCode = () => {
+    console.log("sasa")
+
+  this.setState({
+    showPasswordInput : !this.state.showPasswordInput
+  })
+
+  }
+
+  shakeIcon = () => {
+    this.setState({
+      shakeClass : "h5p-password-icon--shake"
+    })
+    setTimeout((() => {
+      this.setState({
+        shakeClass : ""
+      })
+    }), 500)
   };
   render() {
 //TODO: replace with l10n translation
     return (
       <div className='h5p-password-content' >
-        <div className={"h5p-password-icon-wrapper"}>
-          <span className={`h5p-password-icon ${this.state.unlocked ? "h5p-password-icon-unlock":"h5p-password-icon-lock"}` }/>
+        <div className={`h5p-password-icon-wrapper ${this.state.unlocked ? "h5p-password-icon-wrapper--correct-code" :
+          !this.state.hasClicked ? "" : "h5p-password-icon-wrapper--wrong-code"
+        }` }>
+          <span className={`h5p-password-icon ${this.state.unlocked ? "unlocked" : "locked"}` + ` ${this.state.shakeClass}`}/>
         </div>
         <h1>{this.state.unlocked ? " Åpnet" : "Låst"}</h1>
-        <span className={"h5p-field-description"}>{this.state.unlocked ? " Rommet er nå åpent!" : "Søk i rommet til du finner koden"}</span>
-        <label className={"h5p-label-wrapper"} htmlFor={"field-code-" + this.props.currentInteractionIndex}>
-          <span className={"h5p-field-text"}>{this.props.hint}</span>
-          <input className={"h5p-field-input"} id={"field-code-" + this.props.currentInteractionIndex} placeholder={"Kode"}  ref={(input) => this.input = input}/>
-        </label>
-        <button className={"h5p-password-btn"} onClick={this.handleOnClick}>{this.state.unlocked ? " Gå videre" : "Lås opp"}</button>
-        {
-          this.state.hasClicked && !this.state.unlocked ?  <span>Koden var dessverre feil, prøv igjen</span> :""
-        }
+
+        {<span className={"h5p-field-description"}>
+
+          {this.state.unlocked ? " Rommet er nå åpent!" :
+              !this.state.hasClicked ? "Søk i rommet til du finner koden." : "Koden var dessverre feil, prøv igjen"
+          }
+
+         </span>}
+        <form className={"h5p-wrapper"} onSubmit={this.handleSubmit}>
+          <label className={"h5p-wrapper"} htmlFor={"field-code-" + this.props.currentInteractionIndex}>
+            <span className={"h5p-field-text"}>{this.props.hint}</span>
+            <input type={this.state.showPasswordInput ? "password" : "text"} className={"h5p-field-input"} id={"field-code-" + this.props.currentInteractionIndex} placeholder={"Kode"}  ref={(input) => this.input = input}/>
+            </label>
+          <button className={"h5p-password-btn show-btn"} type={"button"} onClick={this.toggleShowCode}>{this.state.showPasswordInput ? "Vis kode" : "Skjul kode"}</button>
+
+          <button className={"h5p-password-btn"} onClick={this.handleOnClick}>{this.state.unlocked ? " Gå videre" : "Lås opp"}</button>
+        </form>
       </div>
     );
   }
