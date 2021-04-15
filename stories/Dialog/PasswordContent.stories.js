43de78d5476f5c1384fdc0e5f9d11f5576514456
eddiman <edbjorgen@gmail.com>
2021-04-15 15:19:30 +0200

    added storybook for PasswrodContent

---
+++ stories/Dialog/PasswordContent.stories.js
@@ -0,0 +1,47 @@
+// @ts-check
+import React, {useState} from "react";
+import PasswordContent from "../../scripts/components/Dialog/PasswordContent";
+import { defaultAppContext } from "../../.storybook/config/contexts";
+import { H5PContext } from "../../scripts/context/H5PContext";
+export default {
+  title: "PasswordContent",
+  component: PasswordContent,
+  argTypes: {
+    children: { control: { disable: true } },
+    currentInteraction: {
+      unlocked: { control: 'boolean' },
+    }
+  },
+};
+
+const defaultArguments = {
+  currentInteraction: {
+    unlocked: false,
+  },
+  currentInteractionIndex: 0,
+  showInteraction: () => {},
+  handlePassword: () => {},
+  hint: '',
+};
+const Template = (args) => {
+  const [hasClicked, setHasClicked] = useState(false);
+  console.log(args)
+  return(
+    <H5PContext.Provider value={defaultAppContext}>
+      <PasswordContent {...defaultArguments} {...args}/>
+    </H5PContext.Provider>
+  );
+};
+export const Empty = Template.bind({});
+Empty.args = {};
+
+export const Unlocked = Template.bind({});
+Unlocked.args = {
+  currentInteraction: {
+    unlocked: true
+  },
+};
+export const WithHint = Template.bind({});
+WithHint.args = {
+  hint: '123',
+};
