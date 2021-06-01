75a8b2b879fb943f0cdfef4d258015085e436850
Sindre Bøyum <sindre.boyum@gmail.com>
2021-06-01 13:42:17 +0200

    Move 3d render check into its own file This makes it easier to keep track of what parameters controls whether the interaction should be rendered in 2d or 3d

--- scripts/utils/utils.js
+++ scripts/utils/utils.js
@@ -9,3 +9,13 @@ export function createUUID() {
 
   return Math.floor(Math.random() * 10 * numberOfDigits).toString(36).padStart(numberOfDigits, "0");
 }
+
+/**
+ * Determine whether or not the interaction should be rendered in 3d
+ * 
+ * @param {Interaction} interaction
+ * @return {boolean}
+ */
+export function renderIn3d(interaction) {
+  return interaction.label && interaction.label.showAsHotspot;
+}
