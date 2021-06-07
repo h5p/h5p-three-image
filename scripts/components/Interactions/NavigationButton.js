a56bb33bbcc103a9c034e42d383769d364b14fef
Eivin Sellevold <71434505+EivinS@users.noreply.github.com>
2021-06-07 10:57:53 +0200

    Update scripts/components/Interactions/NavigationButton.js
    Co-authored-by: Sindre Bøyum <boyum@users.noreply.github.com>

--- scripts/components/Interactions/NavigationButton.js
+++ scripts/components/Interactions/NavigationButton.js
@@ -293,7 +293,7 @@ export default class NavigationButton extends React.Component {
   }
 
   setFocus(preventCameraMovement = false) {
-    if (preventCameraMovement && !this.props.staticScene  && this.props.staticScene !== undefined) {
+    if (preventCameraMovement && this.context.threeSixty) {
       this.context.threeSixty.setPreventCameraMovement(true);
     }
     const isFocusable = this.context.extras.isEditor
