59914736c028acfd5ac22de2d1800ee3c2f7b730
Sindre Bøyum <sindre.boyum@gmail.com>
2021-12-15 13:36:58 +0100

    decrease hotspot icon size when the hotspot is scaled down

--- scripts/utils/utils.js
+++ scripts/utils/utils.js
@@ -9,3 +9,15 @@
 export function renderIn3d(interaction) {
   return interaction.label && interaction.label.showAsHotspot;
 }
+
+/**
+ * Returns the `value` if it's greater than the minimum and lower than the maximum. 
+ * If it's lower than the minimum, then the minimum is returned, 
+ * else if the value is greater than the maximum, then the maximum is returned.
+ * 
+ * @param {number} min 
+ * @param {number} value 
+ * @param {number} max 
+ * @returns {number}
+ */
+export const clamp = (min, value, max) => Math.min(max, Math.max(min, value));
